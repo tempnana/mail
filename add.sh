2ad0b4663f627cd57a546a711d8e507bf3e1afb1
tempnana <94152736+tempnana@users.noreply.github.com>
2021-12-21 16:04:49 +0800

    Create add.sh

---
+++ add.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+###add domain and user
+user=some_user_name
+password=some_user_password
+cd james-server-app-3.1.0/bin
+cat /root/domain.txt | while read domain
+do
+sh james-cli.sh -h localhost -p 9999 AddDomain $domain
+sleep 1
+sh james-cli.sh -h localhost -p 9999 AddUser $user@$domain $password
+done
+###list domain and user
+# sh james-cli.sh -h localhost -p 9999 ListDomains
+# sh james-cli.sh -h localhost -p 9999 ListUsers
